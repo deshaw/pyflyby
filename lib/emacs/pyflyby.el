bb50b12399fd40504d0211f0a6808cd8af7e1b22
Karl Chen <quarl@quarl.org>
2011-08-29 12:11:11 -0400

    Auto-commit lib/emacs/pyflyby.el [+-] (Emacs@desb0027)

--- lib/emacs/pyflyby.el
+++ lib/emacs/pyflyby.el
@@ -2,7 +2,7 @@
 (defun pyflyby-transform-region-with-command (command)
   (unless (eq major-mode 'python-mode)
     (error "Pyflyby should only be used on python buffers"))
-  (shell-command-on-region (point-min) (point-max) "tidy-imports"
+  (shell-command-on-region (point-min) (point-max) command
                            nil t "*pyflyby*" t))
 
 (defun pyflyby-tidy-imports ()
