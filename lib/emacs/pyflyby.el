eb78a0e58118fc0ad6d98af2fd8acdd7e7b17d71
Karl Chen <quarl@quarl.org>
2011-08-29 16:47:21 -0400

    Auto-commit lib/emacs/pyflyby.el [++++++++++++++++++--] (Emacs@desb0027)

--- lib/emacs/pyflyby.el
+++ lib/emacs/pyflyby.el
@@ -2,8 +2,24 @@
 (defun pyflyby-transform-region-with-command (command)
   (unless (eq major-mode 'python-mode)
     (error "Pyflyby should only be used on python buffers"))
-  (shell-command-on-region (point-min) (point-max) command
-                           nil t "*pyflyby*" t))
+  (let* ((p (point))
+         (text (buffer-substring-no-properties p (min (point-max) (+ p 1000))))
+         (size (buffer-size)))
+    (shell-command-on-region (point-min) (point-max) command
+                             nil t "*pyflyby*" t)
+    ;; Go to previously saved point.  We don't use save-excursion since that
+    ;; doesn't work when the whole buffer is replaced, destroying markers.
+    ;; First see if the position matches when counting from end of the buffer.
+    (setq p (+ p (buffer-size) (- size)))
+    (goto-char p)
+    (unless (search-forward text (+ p 1) t)
+      ;; Next try searching for the location based on text near old point.
+      (goto-char (- p 500))
+      (if (search-forward text (+ p 1000) t)
+          (goto-char (match-beginning 0))
+        (goto-char p)))
+    (recenter)))
+
 
 (defun pyflyby-tidy-imports ()
   (interactive "*")
