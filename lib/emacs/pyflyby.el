51477251882baa831d493edd8440f80724e7423e
Karl Chen <quarl@quarl.org>
2011-09-23 15:17:46 -0400

    Auto-commit lib/emacs/pyflyby.el [58 +++++++++++++++++-------] (univice@desb0027)

--- lib/emacs/pyflyby.el
+++ lib/emacs/pyflyby.el
@@ -1,24 +1,46 @@
 
-(defun pyflyby-transform-region-with-command (command)
+(defun pyflyby-transform-region-with-command (command &rest args)
   (unless (eq major-mode 'python-mode)
     (error "Pyflyby should only be used on python buffers"))
-  (let* ((p (point))
-         (text (buffer-substring-no-properties p (min (point-max) (+ p 1000))))
-         (size (buffer-size)))
-    (shell-command-on-region (point-min) (point-max) command
-                             nil t "*pyflyby*" t)
-    ;; Go to previously saved point.  We don't use save-excursion since that
-    ;; doesn't work when the whole buffer is replaced, destroying markers.
-    ;; First see if the position matches when counting from end of the buffer.
-    (setq p (+ p (buffer-size) (- size)))
-    (goto-char p)
-    (unless (search-forward text (+ p 1) t)
-      ;; Next try searching for the location based on text near old point.
-      (goto-char (- p 500))
-      (if (search-forward text (+ p 1000) t)
-          (goto-char (match-beginning 0))
-        (goto-char p)))
-    (recenter)))
+  (let* ((output-buffer (generate-new-buffer "*pyflyby output*"))
+         (exit-value (apply 'call-process-region
+                            (point-min) (point-max) command nil
+                            (list output-buffer "*pyflyby log*")
+                            nil args)))
+    (if (= exit-value 0)
+        ;; Process exited successfully.
+        (let ((newtext
+               (with-current-buffer output-buffer
+                 (buffer-substring-no-properties (point-min) (point-max)))))
+          (if (string= newtext
+                       (buffer-substring-no-properties (point-min) (point-max)))
+              ;; No change.
+              (message "No changes by `%s'" command)
+            ;; There were changes.
+            (let* ((p (point))
+                   (ptext (buffer-substring-no-properties
+                           p (min (point-max) (+ p 1000))))
+                   (size (buffer-size)))
+              ;; Replace the buffer.
+              (erase-buffer)
+              (insert newtext)
+              ;; Go to previously saved point.  We don't use save-excursion
+              ;; since that doesn't work when the whole buffer is replaced,
+              ;; destroying markers.  First see if the position matches when
+              ;; counting from end of the buffer.
+              (setq p (+ p (buffer-size) (- size)))
+              (goto-char p)
+              (unless (search-forward ptext (+ p 1) t)
+                ;; Next, try searching for the location based on text near old
+                ;; point.
+                (goto-char (- p 500))
+                (if (search-forward ptext (+ p 1000) t)
+                    (goto-char (match-beginning 0))
+                  (goto-char p)))
+              (recenter))))
+      ;; Process failed.
+      (error "%s failed with exit code %d" command exit-value))
+    (kill-buffer output-buffer)))
 
 
 (defun pyflyby-tidy-imports ()
