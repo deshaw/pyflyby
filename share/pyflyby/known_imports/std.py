4d2249635eb03b39e4bf163696ffd65b4d569ee0
Karl Chen <quarl@quarl.org>
2012-04-12 18:16:51 -0400

    Auto-commit share/pyflyby/known_imports/std.py [161 +++++++++++++++++++++++---] (Emacs@desb0448)

--- share/pyflyby/known_imports/std.py
+++ share/pyflyby/known_imports/std.py
@@ -70,11 +70,11 @@ import heapq
 import httplib
 import imp
 import inspect
-from   inspect                  import getargspec
+from   inspect                  import ArgSpec, getargspec
 import io
 import itertools
 from   itertools                import (chain, count, groupby, imap, islice,
-                                        izip, product, repeat)
+                                        izip, product, repeat, tee)
 import json
 import kerberos
 from   keyword                  import iskeyword
@@ -90,7 +90,8 @@ import misc.double
 import mmap
 import new
 import numbers
-from   numbers                  import Number
+from   numbers                  import (Complex, Integral, Number, Rational,
+                                        Real)
 import operator
 from   operator                 import add, indexOf, itemgetter, mul
 import optparse
@@ -107,7 +108,7 @@ import parser
 import pdb
 import perl
 import pickle
-from   pickle                   import PickleError
+from   pickle                   import PickleError, UnpicklingError
 import pkg_resources
 import pprint
 import pstats
@@ -123,33 +124,157 @@ import resource
 import select
 import shlex
 import shutil
-from   shutil                   import rmtree
+from   shutil                   import (copyfile, copyfileobj, copystat,
+                                        copytree, rmtree)
 import signal
 import smtplib
-from   smtplib                  import SMTP
+from   smtplib                  import (SMTP, SMTPAuthenticationError,
+                                        SMTPConnectError, SMTPDataError,
+                                        SMTPException, SMTPHeloError,
+                                        SMTPRecipientsRefused,
+                                        SMTPResponseException,
+                                        SMTPSenderRefused,
+                                        SMTPServerDisconnected, SMTP_SSL)
 import socket
-from   socket                   import AF_INET, SOCK_STREAM, gethostname
+from   socket                   import (AF_APPLETALK, AF_ASH, AF_ATMPVC,
+                                        AF_ATMSVC, AF_AX25, AF_BRIDGE,
+                                        AF_DECnet, AF_ECONET, AF_INET,
+                                        AF_INET6, AF_IPX, AF_IRDA, AF_KEY,
+                                        AF_LLC, AF_NETBEUI, AF_NETLINK,
+                                        AF_NETROM, AF_PACKET, AF_PPPOX,
+                                        AF_ROSE, AF_ROUTE, AF_SECURITY, AF_SNA,
+                                        AF_TIPC, AF_UNIX, AF_UNSPEC,
+                                        AF_WANPIPE, AF_X25, AI_ADDRCONFIG,
+                                        AI_ALL, AI_CANONNAME, AI_NUMERICHOST,
+                                        AI_NUMERICSERV, AI_PASSIVE,
+                                        AI_V4MAPPED, CAPI, EAI_ADDRFAMILY,
+                                        EAI_AGAIN, EAI_BADFLAGS, EAI_FAIL,
+                                        EAI_FAMILY, EAI_MEMORY, EAI_NODATA,
+                                        EAI_NONAME, EAI_OVERFLOW, EAI_SERVICE,
+                                        EAI_SOCKTYPE, EAI_SYSTEM,
+                                        INADDR_ALLHOSTS_GROUP, INADDR_ANY,
+                                        INADDR_BROADCAST, INADDR_LOOPBACK,
+                                        INADDR_MAX_LOCAL_GROUP, INADDR_NONE,
+                                        INADDR_UNSPEC_GROUP, IPPORT_RESERVED,
+                                        IPPORT_USERRESERVED, IPPROTO_AH,
+                                        IPPROTO_DSTOPTS, IPPROTO_EGP,
+                                        IPPROTO_ESP, IPPROTO_FRAGMENT,
+                                        IPPROTO_GRE, IPPROTO_HOPOPTS,
+                                        IPPROTO_ICMP, IPPROTO_ICMPV6,
+                                        IPPROTO_IDP, IPPROTO_IGMP, IPPROTO_IP,
+                                        IPPROTO_IPIP, IPPROTO_IPV6,
+                                        IPPROTO_NONE, IPPROTO_PIM, IPPROTO_PUP,
+                                        IPPROTO_RAW, IPPROTO_ROUTING,
+                                        IPPROTO_RSVP, IPPROTO_TCP, IPPROTO_TP,
+                                        IPPROTO_UDP, IPV6_CHECKSUM,
+                                        IPV6_DSTOPTS, IPV6_HOPLIMIT,
+                                        IPV6_HOPOPTS, IPV6_JOIN_GROUP,
+                                        IPV6_LEAVE_GROUP, IPV6_MULTICAST_HOPS,
+                                        IPV6_MULTICAST_IF, IPV6_MULTICAST_LOOP,
+                                        IPV6_NEXTHOP, IPV6_PKTINFO,
+                                        IPV6_RECVDSTOPTS, IPV6_RECVHOPLIMIT,
+                                        IPV6_RECVHOPOPTS, IPV6_RECVPKTINFO,
+                                        IPV6_RECVRTHDR, IPV6_RECVTCLASS,
+                                        IPV6_RTHDR, IPV6_RTHDRDSTOPTS,
+                                        IPV6_RTHDR_TYPE_0, IPV6_TCLASS,
+                                        IPV6_UNICAST_HOPS, IPV6_V6ONLY,
+                                        IP_ADD_MEMBERSHIP,
+                                        IP_DEFAULT_MULTICAST_LOOP,
+                                        IP_DEFAULT_MULTICAST_TTL,
+                                        IP_DROP_MEMBERSHIP, IP_HDRINCL,
+                                        IP_MAX_MEMBERSHIPS, IP_MULTICAST_IF,
+                                        IP_MULTICAST_LOOP, IP_MULTICAST_TTL,
+                                        IP_OPTIONS, IP_RECVOPTS,
+                                        IP_RECVRETOPTS, IP_RETOPTS, IP_TOS,
+                                        IP_TTL, MSG_CTRUNC, MSG_DONTROUTE,
+                                        MSG_DONTWAIT, MSG_EOR, MSG_OOB,
+                                        MSG_PEEK, MSG_TRUNC, MSG_WAITALL,
+                                        NETLINK_DNRTMSG, NETLINK_FIREWALL,
+                                        NETLINK_IP6_FW, NETLINK_NFLOG,
+                                        NETLINK_ROUTE, NETLINK_USERSOCK,
+                                        NETLINK_XFRM, NI_DGRAM, NI_MAXHOST,
+                                        NI_MAXSERV, NI_NAMEREQD, NI_NOFQDN,
+                                        NI_NUMERICHOST, NI_NUMERICSERV,
+                                        PACKET_BROADCAST, PACKET_FASTROUTE,
+                                        PACKET_HOST, PACKET_LOOPBACK,
+                                        PACKET_MULTICAST, PACKET_OTHERHOST,
+                                        PACKET_OUTGOING, PF_PACKET, SHUT_RD,
+                                        SHUT_RDWR, SHUT_WR, SOCK_DGRAM,
+                                        SOCK_RAW, SOCK_RDM, SOCK_SEQPACKET,
+                                        SOCK_STREAM, SOL_IP, SOL_SOCKET,
+                                        SOL_TCP, SOL_TIPC, SOL_UDP, SOMAXCONN,
+                                        SO_ACCEPTCONN, SO_ATTACH_FILTER,
+                                        SO_BINDTODEVICE, SO_BROADCAST,
+                                        SO_BSDCOMPAT, SO_DEBUG,
+                                        SO_DETACH_FILTER, SO_DONTROUTE,
+                                        SO_ERROR, SO_KEEPALIVE, SO_LINGER,
+                                        SO_NO_CHECK, SO_OOBINLINE, SO_PASSCRED,
+                                        SO_PASSSEC, SO_PEERCRED, SO_PEERNAME,
+                                        SO_PEERSEC, SO_PRIORITY, SO_RCVBUF,
+                                        SO_RCVBUFFORCE, SO_RCVLOWAT,
+                                        SO_RCVTIMEO, SO_REUSEADDR,
+                                        SO_SECURITY_AUTHENTICATION,
+                                        SO_SECURITY_ENCRYPTION_NETWORK,
+                                        SO_SECURITY_ENCRYPTION_TRANSPORT,
+                                        SO_SNDBUF, SO_SNDBUFFORCE, SO_SNDLOWAT,
+                                        SO_SNDTIMEO, SO_TIMESTAMP,
+                                        SO_TIMESTAMPNS, SO_TYPE, SocketType,
+                                        TCP_CONGESTION, TCP_CORK,
+                                        TCP_DEFER_ACCEPT, TCP_INFO,
+                                        TCP_KEEPCNT, TCP_KEEPIDLE,
+                                        TCP_KEEPINTVL, TCP_LINGER2, TCP_MAXSEG,
+                                        TCP_MD5SIG, TCP_MD5SIG_MAXKEYLEN,
+                                        TCP_NODELAY, TCP_QUICKACK, TCP_SYNCNT,
+                                        TCP_WINDOW_CLAMP, TIPC_ADDR_ID,
+                                        TIPC_ADDR_NAME, TIPC_ADDR_NAMESEQ,
+                                        TIPC_CFG_SRV, TIPC_CLUSTER_SCOPE,
+                                        TIPC_CONN_TIMEOUT,
+                                        TIPC_CRITICAL_IMPORTANCE,
+                                        TIPC_DEST_DROPPABLE,
+                                        TIPC_HIGH_IMPORTANCE, TIPC_IMPORTANCE,
+                                        TIPC_LOW_IMPORTANCE,
+                                        TIPC_MEDIUM_IMPORTANCE,
+                                        TIPC_NODE_SCOPE, TIPC_PUBLISHED,
+                                        TIPC_SRC_DROPPABLE,
+                                        TIPC_SUBSCR_TIMEOUT, TIPC_SUB_CANCEL,
+                                        TIPC_SUB_PORTS, TIPC_SUB_SERVICE,
+                                        TIPC_TOP_SRV, TIPC_WAIT_FOREVER,
+                                        TIPC_WITHDRAWN, TIPC_ZONE_SCOPE,
+                                        gaierror, getaddrinfo, getfqdn,
+                                        gethostbyaddr, gethostbyname,
+                                        gethostbyname_ex, gethostname,
+                                        getnameinfo, getprotobyname,
+                                        getservbyname, getservbyport, htonl,
+                                        htons, inet_aton, inet_ntoa, inet_ntop,
+                                        inet_pton, ntohl, ntohs,
+                                        setdefaulttimeout, socketpair)
 import sqlite3
 import stat
-from   stat                     import ST_MTIME
+from   stat                     import (ST_MTIME, S_IFMT, S_IMODE, S_ISBLK,
+                                        S_ISCHR, S_ISDIR, S_ISFIFO, S_ISLNK,
+                                        S_ISREG, S_ISSOCK)
 import string
 import struct
 import subprocess
-from   subprocess               import PIPE, Popen
+from   subprocess               import (CalledProcessError, PIPE, Popen, call,
+                                        check_call)
 import symbol
 import sys
-from   sys                      import exit, stderr, stdout
+from   sys                      import exit, getsizeof, stderr, stdout
 import tempfile
-from   tempfile                 import (NamedTemporaryFile, mkdtemp, mkstemp,
-                                        mktemp)
+from   tempfile                 import (NamedTemporaryFile,
+                                        SpooledTemporaryFile, TemporaryFile,
+                                        mkdtemp, mkstemp, mktemp)
 import termios
 import textwrap
 from   textwrap                 import dedent
 import threading
-from   threading                import (Condition, Thread, currentThread,
-                                        current_thread)
+from   threading                import (BoundedSemaphore, Condition, Lock,
+                                        RLock, Semaphore, Thread, Timer,
+                                        currentThread, current_thread)
 import time
-from   time                     import gmtime, mktime, sleep, strftime
+from   time                     import (ctime, gmtime, localtime, mktime,
+                                        sleep, strftime, strptime)
 import timeit
 import token
 import traceback
@@ -162,7 +287,11 @@ import urllib2
 from   urllib2                  import urlopen
 import warnings
 import weakref
-from   weakref                  import WeakKeyDictionary, WeakValueDictionary
+from   weakref                  import (CallableProxyType, ProxyType,
+                                        ProxyTypes, ReferenceError,
+                                        ReferenceType, WeakKeyDictionary,
+                                        WeakValueDictionary, getweakrefcount,
+                                        getweakrefs)
 import xlrd
 from   xml.dom                  import minidom
 import xml.parsers.expat
