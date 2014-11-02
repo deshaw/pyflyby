e5d9e85d5860209c4be286da852ab980c1ba48d4
Karl Chen <quarl@quarl.org>
2014-11-02 15:16:18 -0500

    Move admin/* into setup.py.

--- setup.py
+++ setup.py
@@ -9,22 +9,103 @@ from __future__ import absolute_import, division, with_statement
 
 import glob
 import os
-from   setuptools               import setup
+import re
+from   setuptools               import Command, setup
 from   setuptools.command.test  import test as TestCommand
+import subprocess
 import sys
+from   textwrap                 import dedent
 
-PYFLYBY_HOME = os.path.dirname(__file__)
-PYFLYBY_PYPATH = os.path.join(PYFLYBY_HOME, "lib/python")
 
+PYFLYBY_HOME        = os.path.dirname(__file__)
+PYFLYBY_PYPATH      = os.path.join(PYFLYBY_HOME, "lib/python")
+PYFLYBY_DOT_PYFLYBY = os.path.join(PYFLYBY_HOME, ".pyflyby")
+
+# Get the pyflyby version from pyflyby.__version__.
 version_vars = {}
 version_fn = os.path.join(PYFLYBY_PYPATH, "pyflyby/_version.py")
 execfile(version_fn, {}, version_vars)
 version = version_vars["__version__"]
 
+
 def read(fname):
     with open(os.path.join(PYFLYBY_HOME, fname)) as f:
         return f.read()
 
+
+def list_python_source_files():
+    results = []
+    for fn in glob.glob("bin/*"):
+        if not os.path.isfile(fn):
+            continue
+        with open(fn) as f:
+            line = f.readline()
+            if not re.match("^#!.*python", line):
+                continue
+        results.append(fn)
+    results += glob.glob("lib/python/pyflyby/*.py")
+    results += glob.glob("tests/*.py")
+    return results
+
+
+class TidyImports(Command):
+    description = "tidy imports in pyflyby source files (for maintainer use)"
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        files = list_python_source_files()
+        pyflyby_path = ":".join([
+            os.path.join(PYFLYBY_HOME, "etc/pyflyby"),
+            PYFLYBY_DOT_PYFLYBY,
+            ])
+        subprocess.call([
+            "env",
+            "PYFLYBY_PATH=%s" % (pyflyby_path,),
+            "tidy-imports",
+            # "--debug",
+            "--uniform",
+            ] + files)
+
+
+class CollectImports(Command):
+    description = "update pyflyby's own .pyflyby file from imports (for maintainer use)"
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        files = list_python_source_files()
+        print "Rewriting", PYFLYBY_DOT_PYFLYBY
+        with open(PYFLYBY_DOT_PYFLYBY, 'w') as f:
+            print >>f, dedent("""
+                # -*- python -*-
+                #
+                # This is the imports database file for pyflyby itself.
+                #
+                # To regenerate this file, run: setup.py collect_imports
+
+            """).lstrip()
+            f.flush()
+            subprocess.call(
+                [
+                    os.path.join(PYFLYBY_HOME, "bin/collect-imports"),
+                    "--include=pyflyby",
+                    "--uniform",
+                ] + files,
+                stdout=f)
+        subprocess.call(["git", "diff", PYFLYBY_DOT_PYFLYBY])
 
 
 class PyTest(TestCommand):
@@ -61,7 +142,6 @@ setup(
     scripts=[
         'bin/collect-exports',
         'bin/collect-imports',
-        'bin/create-imports',
         'bin/find-import',
         'bin/list-bad-xrefs',
         'bin/prune-broken-imports',
@@ -91,4 +171,9 @@ setup(
     ],
     install_requires=['pyflakes'],
     tests_require=['pytest', 'epydoc'],
-    cmdclass = {'test': PyTest},)
+    cmdclass = {
+        'test'           : PyTest,
+        'collect_imports': CollectImports,
+        'tidy_imports'   : TidyImports,
+    },
+)
