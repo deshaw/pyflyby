45ddbaf545e1e27b60c969bddfd401d1750a8dbd
Karl Chen <quarl@quarl.org>
2014-11-17 22:24:50 -0500

    Don't export load_symbol

--- lib/python/pyflyby/__init__.py
+++ lib/python/pyflyby/__init__.py
@@ -5,7 +5,7 @@
 from __future__ import absolute_import, division, with_statement
 
 from   pyflyby._autoimp         import (auto_eval, auto_import,
-                                        find_missing_imports, load_symbol)
+                                        find_missing_imports)
 from   pyflyby._file            import Filename
 from   pyflyby._flags           import CompilerFlags
 from   pyflyby._importdb        import ImportDB
