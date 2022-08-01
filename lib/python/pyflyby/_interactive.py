a4f4359de5a0980f56463bd7c1d87c664dac72cc
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-08-01 12:05:34 +0200

    Fix #197 – IOStream Removed.
    Using IOStream should be unnecessary. Just do Nothing on IPython 8 and
    above. I believe could be way less conservative and don't use IOStream
    for IPython 5 and above, but better be safe

--- lib/python/pyflyby/_interactive.py
+++ lib/python/pyflyby/_interactive.py
@@ -1329,6 +1329,13 @@ def UpdateIPythonStdioCtx():
     if "IPython" not in sys.modules:
         yield
         return
+
+    import IPython
+
+    if IPython.version_info[:1] >= (8,):
+        yield
+        return
+
     if "IPython.utils.io" in sys.modules:
         # Tested with IPython 0.11, 0.12, 0.13, 1.0, 1.1, 1.2, 2.0, 2.1, 2.2,
         # 2.3, 2.4, 3.0, 3.1, 3.2, 4.0.
