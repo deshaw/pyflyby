ee751d3fb0b209121350b5b79a654f4c659f1b66
Peyton Murray <peynmurray@gmail.com>
2025-08-27 22:48:47 -0700

    [ENH] Speed up first completion (#399)
    * Remove python 2 compat junk
    
    * WIP
    
    * Cut as many corners as possible while still remaining with python
    
    * Fix type annotation
    
    * cpp extension boilerplate
    
    * meson build is now working
    
    * WIP
    
    * wip
    
    * Implement a compiled extension which finds modules
    
    * Compiled extension tested, working
    
    * Don't use `python setup.py <whatever>`
    
    * Don't use editable installs for testing
    
    * Don't build/test with build isolation
    
    * Switch order of args
    
    * Correct a bad docstring
    
    * Add pybind11 dep to CI...
    
    * Enforce c++17 standard
    
    * Fix the doc build configuration
    
    * Use dependency groups for docs and lint; don't build docs in test
    
    * Add 'build' as test dependency
    
    * Make tests run in editable mode
    
    * Add hypothesis; follow meson-python docs around editable mode
    
    * Make epydoc and wheel direct dependencies of pyflyby
    
    * Add setuptools and wheel, required by epydoc
    
    * Make the install `pure: false`; prevent site-packages pollution
    
    * Set lower bound on meson-python to 0.18.0 to enforce PEP639
    
    * Add caching; progress bars aren't quite right yet
    
    * Don't bother with progress bars
    
    * Add a function to force the cache to be rebuilt
    
    * Export rebuild_import_cache at top level
    
    * Cleanup
    
    * Expand import caching test
    
    * Check that updating the mtime of an importer path regenerates the cache
    
    * Add `appdirs` to autodoc_mock_imports; remove unused import
    
    * Add `prompt-toolkit` as dependency
    
    * Fix tests broken by new log messages
    
    * Disable test broken due to log messages
    
    * Remove patch_stdout calls; that's a problem for another time
    
    * Try using two scenarios
    
    * Try stripping out log lines; can't do anything else since subprocess...
    
    * Add env variable for suppressing pyflyby cache rebuild log messages

--- doc/conf.py
+++ doc/conf.py
@@ -2,6 +2,8 @@
 
 # -- Path setup --------------------------------------------------------------
 import os
+import pathlib
+import re
 import sys
 sys.path.insert(0, os.path.abspath('../lib/python'))
 sys.path.insert(0, os.path.abspath('..'))
@@ -11,9 +13,21 @@ project = 'pyflyby'
 copyright = '2019, Karl Chen'
 author = 'Karl Chen'
 # The full version, including alpha/beta/rc tags
-import pyflyby
 
-release = pyflyby.__version__
+def find_version():
+    # Extract version information via regex to avoid importing
+    project_root = pathlib.Path(__file__).parent.parent
+    with open(project_root / "lib" / "python" / "pyflyby" / "_version.py") as f:
+        version_match = re.search(
+            r"^__version__ = ['\"](?P<version>.*)['\"]$",
+            f.read(),
+            re.M,
+        )
+        if version_match:
+            return version_match.group("version")
+        raise RuntimeError("Unable to find version string.")
+
+release = find_version()
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,6 +42,12 @@ autodoc_default_options = {
     'undoc-members': True,
     'private-members': True
 }
+
+autodoc_mock_imports = [
+    "pyflyby._fast_iter_modules",
+    "appdirs",
+    "prompt_toolkit",
+]
 
 html_theme_options = {
     'collapse_navigation': False,
