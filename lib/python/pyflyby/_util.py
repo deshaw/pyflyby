4596bf25ef649c6812afe6469e8b34294869728e
M Bussonnier <bussonniermatthias@gmail.com>
2025-12-16 12:32:19 +0100

    add cached_attribute to __all__ to avoid import remove

--- lib/python/pyflyby/_util.py
+++ lib/python/pyflyby/_util.py
@@ -9,14 +9,14 @@ import inspect
 import os
 import sys
 import types
+from types import MappingProxyType as DictProxyType
 
 # There used to be a custom caching_attribute implementation
 # this now uses functools's cached_property which is understood by
 # various static analysis tools.
 from functools import cached_property as cached_attribute # noqa: F401
 
-# Python 2/3 compatibility
-DictProxyType = type(object.__dict__)
+__all__ = ["cached_attribute"]
 
 if sys.version_info > (3,9):
     from functools import cache as memoize
