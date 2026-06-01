6774454bb3a25ffd449b00a3fc7647c770d15e52
M Bussonnier <bussonniermatthias@gmail.com>
2026-06-01 14:51:19 +0200

    Remove dead code and a number of old-ipython compatibility branches. (#503)
    Most of these should be completely unused and the version of IPython
    they target will not wor with the minimum Python we support.

--- lib/python/pyflyby/importdb.py
+++ lib/python/pyflyby/importdb.py
@@ -6,15 +6,29 @@
 
 from __future__ import annotations, print_function
 
+import warnings
+
 from   pyflyby._importclns      import ImportSet
 from   pyflyby._importdb        import ImportDB
 
 
 def global_known_imports() -> ImportSet:
     # Deprecated stub for backwards compatibility.
+    warnings.warn(
+        "global_known_imports() has been deprecated since 2014-10; "
+        "use ImportDB.get_default('.').known_imports instead.",
+        DeprecationWarning,
+        stacklevel=2,
+    )
     return ImportDB.get_default(".").known_imports
 
 
 def global_mandatory_imports() -> ImportSet:
     # Deprecated stub for backwards compatibility.
+    warnings.warn(
+        "global_mandatory_imports() has been deprecated since 2014-10; "
+        "use ImportDB.get_default('.').mandatory_imports instead.",
+        DeprecationWarning,
+        stacklevel=2,
+    )
     return ImportDB.get_default(".").mandatory_imports
